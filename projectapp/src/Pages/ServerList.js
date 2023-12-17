5161ad9ecb3f33b8cc1219eb6fbca11ab40c76e2
Mauger Florian <florian.mauger@etu.umontpellier.fr>
2023-12-17 20:12:11 +0000

    Replace ServerList.js

--- projectapp/src/Pages/ServerList.js
+++ projectapp/src/Pages/ServerList.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { ButtonGroup, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CSS/card.css';
+// import './CSS/card.css';
 
 const ServerList = ({servers, handleClick, handlePassword}) => {
-  
+  const joinServer = (id) => {
+    console.log(`Joining server ${id}`);
+    // Ajoutez votre logique de redirection ou d'action de rejoindre le serveur ici
+  };
 
   return (
-    <div className="card-container">
+<div className="server-container">
       {servers.map((server) => (
-        <div key={server.id} className="card">
+        <div key={server.id} className="submitServer">
+        <div className="text-card">
           <p>{server.serverName}{"(" + server.playerList.length + "/" + server.nbPlayerMax + ")"}</p>
+          </div>
           {// eslint-disable-next-line
            (server.playerList.length) != server.nbPlayerMax && server.isPrivate && (
+            <div className='input password'>
             <input
               type="password"
-              placeholder="Entrez le mot de passe"
+              placeholder="password..."
               onChange={(e) => {handlePassword(e.target.value)}}
             />
+            </div>
           )}
-          <button onClick={() => handleClick(server.id,server)} disabled={// eslint-disable-next-line
+          <button class="submit small"onClick={() => handleClick(server.id,server)} disabled={// eslint-disable-next-line
           (server.playerList.length) == server.nbPlayerMax}>
             {// eslint-disable-next-line
             server.playerList.length == server.nbPlayerMax ? 'FULL' : 'Rejoindre'}
@@ -28,5 +36,8 @@ const ServerList = ({servers, handleClick, handlePassword}) => {
     </div>
   );
 };
+
+
+
 
 export default ServerList;

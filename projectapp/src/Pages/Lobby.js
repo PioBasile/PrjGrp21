389ca70e44a67ed5c4e0c8d0113810eda175991f
Mauger Florian <florian.mauger@etu.umontpellier.fr>
2024-03-04 20:29:08 +0000

    Update Lobby.js

--- projectapp/src/Pages/Lobby.js
+++ projectapp/src/Pages/Lobby.js
@@ -1,50 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import './CSS/LobbyStyleSheet.css'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { Button, ListGroup, Form } from 'react-bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, ListGroup, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import socket from '../socketG';
 
 
 const Lobby = () => {
-  
+
   socket.emit('join', sessionStorage.getItem('serverConnected'));
-  sessionStorage.setItem('loaded',false);
-  const [playerList, setPlayerList] = useState([ ]);
+  sessionStorage.setItem('loaded', false);
+  const [playerList, setPlayerList] = useState([]);
   const [maxPlayers, setMaxPlayers] = useState(10);
   const [timeBetweenTurn, setTimeBetweenTurn] = useState(30);
   const [roundsMax, setRoundsMax] = useState(20);
   const navigate = useNavigate();
-  const [clobby, setLobby] = useState({playerList:[]});
+  const [clobby, setLobby] = useState({ playerList: [] });
 
-  function setPlayer(maxP){
+  function setPlayer(maxP) {
 
     setMaxPlayers(maxP);
-    socket.emit('updateParam',sessionStorage.getItem('serverConnected') ,maxP, timeBetweenTurn, roundsMax);
+    socket.emit('updateParam', sessionStorage.getItem('serverConnected'), maxP, timeBetweenTurn, roundsMax);
 
   };
 
-  function setRound(maxR){
+  function setRound(maxR) {
 
     setRoundsMax(maxR);
-    socket.emit('updateParam',sessionStorage.getItem('serverConnected') ,maxPlayers, timeBetweenTurn, maxR);
+    socket.emit('updateParam', sessionStorage.getItem('serverConnected'), maxPlayers, timeBetweenTurn, maxR);
 
   };
 
-  function setTimeTurn(time){
+  function setTimeTurn(time) {
 
     setTimeBetweenTurn(time)
-    socket.emit('updateParam',sessionStorage.getItem('serverConnected') ,maxPlayers, time, roundsMax);
+    socket.emit('updateParam', sessionStorage.getItem('serverConnected'), maxPlayers, time, roundsMax);
 
+    return () => {
+      socket.off("failure");
+      socket.off("succes");
+    }
   };
 
 
   const handleKickPlayer = (index) => {
-    socket.emit('deco_lobby', sessionStorage.getItem('serverConnected') ,playerList[index].username);
+    socket.emit('deco_lobby', sessionStorage.getItem('serverConnected'), playerList[index].username);
   };
 
   const handleReadyClick = () => {
-    socket.emit('ready', sessionStorage.getItem('serverConnected'),sessionStorage.getItem('name'));
+    socket.emit('ready', sessionStorage.getItem('serverConnected'), sessionStorage.getItem('name'));
   };
 
   const handleDisconnectClick = () => {
@@ -54,20 +58,20 @@ const Lobby = () => {
 
   const handleStart = () => {
 
-      let count = 0;
-      clobby.playerList.forEach(player => {
-        if(player.isReady){count++;}
-      });
+    let count = 0;
+    clobby.playerList.forEach(player => {
+      if (player.isReady) { count++; }
+    });
 
-      if(count === clobby.nbPlayerMax && clobby.owner === sessionStorage.getItem('name')){
-        socket.emit("StartGame", sessionStorage.getItem('serverConnected'));
-      };
+    if (count === clobby.nbPlayerMax && clobby.owner === sessionStorage.getItem('name')) {
+      socket.emit("StartGame", sessionStorage.getItem('serverConnected'));
+    };
 
   }
 
   useEffect(() => {
 
-    if(sessionStorage.getItem('serverConnected')==="-1"){
+    if (sessionStorage.getItem('serverConnected') === "-1") {
       navigate("/BrowserManager");
     }
 
@@ -95,67 +99,67 @@ const Lobby = () => {
     // -----------------
 
 
-    if(sessionStorage.getItem('loaded') === "true"){return;}else{
-      socket.emit('WhereAmI',sessionStorage.getItem('serverConnected'));sessionStorage.setItem('loaded',true)
+    if (sessionStorage.getItem('loaded') === "true") { return; } else {
+      socket.emit('WhereAmI', sessionStorage.getItem('serverConnected')); sessionStorage.setItem('loaded', true)
     }
-    socket.on('here',(lobby) => {
-      if(mounted){
-      setLobby(lobby);
-      setPlayerList(lobby.playerList);
-      setMaxPlayers(lobby.nbPlayerMax);
+    socket.on('here', (lobby) => {
+      if (mounted) {
+        setLobby(lobby);
+        setPlayerList(lobby.playerList);
+        setMaxPlayers(lobby.nbPlayerMax);
 
-    }
-      
+      }
+
 
     });
 
     socket.on("lobbyParams", (maxPlayers, timeBetweenTurn, roundsMax) => {
 
-      if(clobby.owner === sessionStorage.getItem('name')){
+      if (clobby.owner === sessionStorage.getItem('name')) {
         return
       } else {
-        if(mounted){
+        if (mounted) {
           setMaxPlayers(maxPlayers);
           setTimeBetweenTurn(timeBetweenTurn);
           setRoundsMax(roundsMax);
         }
-        
-      }
-      
 
-    }); 
+      }
+
+
+    });
 
     socket.on('disconected', (name) => {
 
-      if(sessionStorage.getItem("name") === name){
+      if (sessionStorage.getItem("name") === name) {
         navigate('/BrowserManager');
 
       }
 
     });
 
-    socket.on("start" , (place) => {
+    socket.on("start", (place) => {
 
-      
+
       navigate(`/${place}`);
 
     });
 
-    
-    return () => {mounted = false};
 
-  }, [clobby.owner,navigate]);
+    return () => { mounted = false };
+
+  }, [clobby.owner, navigate]);
 
 
   return (
-<div className="app-container">
+    <div className="app-container">
       <div className="left-panel">
         <h2 className="text-white">Joueurs</h2>
         <ListGroup>
           {playerList.map((player, index) => (
             <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center">
-              {index+1 + " "}
-              {player.username + " | " + (player.isReady? "Pret" : "Pas pret")}
+              {index + 1 + " "}
+              {player.username + " | " + (player.isReady ? "Pret" : "Pas pret")}
               <Button variant="danger" size="sm" disabled={player.username === sessionStorage.getItem('name') || clobby.owner !== sessionStorage.getItem('name')} onClick={() => handleKickPlayer(index)}>
                 Kick
               </Button>

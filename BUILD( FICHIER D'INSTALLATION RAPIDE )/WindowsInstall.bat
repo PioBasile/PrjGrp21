d74cbeaedf57f84ef6d39f43ec21099a4d0f4309
Rey Dorian <dorian.rey01@etu.umontpellier.fr>
2024-03-05 22:29:25 +0100

    okay

--- BUILD( FICHIER D'INSTALLATION RAPIDE )/WindowsInstall.bat
+++ BUILD( FICHIER D'INSTALLATION RAPIDE )/WindowsInstall.bat
@@ -4,7 +4,7 @@ call npm install express
 call npm install sqlite3 
 call npm install socket.io 
 call cd projectapp 
-call npm install react-scripts -f
+call npm install --prefix ./ react-scripts -f
 call cd ..
 call cd ".\BUILD( FICHIER D'INSTALLATION RAPIDE )"
 call windowsQuickStart.bat

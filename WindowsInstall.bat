0f215270878d88b7d1a9afa80efa18aeddb8f837
Rey Dorian <dorian.rey01@etu.umontpellier.fr>
2024-03-05 18:32:12 +0100

    exec

--- WindowsInstall.bat
+++ WindowsInstall.bat
@@ -1,5 +1,8 @@
+@ECHO OFF 
 call npm install express 
 call npm install sqlite3 
 call npm install socket.io 
 call cd projectapp 
 call npm install react-scripts -f
+call cd ..
+call windowsQuickStart.bat

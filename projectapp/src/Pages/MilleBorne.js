560b1ab10d3a6da694e13612aae0eaf5c4ba1717
uni-git <francesco-pio.basile@etu.umontpellier.fr>
2024-03-16 15:17:30 +0100

    ajout de info

--- projectapp/src/Pages/MilleBorne.js
+++ projectapp/src/Pages/MilleBorne.js
@@ -34,10 +34,16 @@ const MilleBorne = () => {
     const [nbCards, setNbCards] = useState(0);
     const [test, setTest] = useState("");
     const [isVisible, setIsVisible] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
+
     
     const getCard = (card) => {
         return cartes.indexOf(card);
     }
+
+    const toggleInfo = () => {
+        setShowInfo(prevShowInfo => !prevShowInfo);
+    };
     
     const sendMessage = () => {
         socket.emit('sendMessage', { name: sessionStorage.getItem("name"), msg: message, serverId: sessionStorage.getItem("serverConnected") });
@@ -269,7 +275,69 @@ const MilleBorne = () => {
                 <p></p>
                 <div></div>
                 <div className='' onClick={() => saveGame()}> SAVE</div>
-                <div className='MB-info-button'> INFO</div>
+                
+                
+                <div className='MB-info-button' onClick={toggleInfo}>INFO</div>
+                    {showInfo && (
+                        <div className='MB-info-content'>
+                        <p> 
+                            <strong>Mille Borne</strong> <br /> <br />
+                            <strong> Description</strong> 
+                            <p>
+                                Le jeu comprend 106 cartes. Les joueurs doivent accumuler des bornes. Il faut aller jusqu'à 1 000 bornes exactement pour que la partie se termine. Le jeu comporte donc des cartes représentant :
+                                les distances parcourues ;
+                                les attaques, c'est-à-dire des aléas de la route (accidents, crevaisons, pannes...), et la signalisation routière (limitation de vitesse, feux) qui permettent de freiner ou stopper son adversaire ;
+                                les parades, qui permettent de contrer l'effet des attaques ;
+                                les bottes, qui sont des protections contre une attaque spécifique, au nombre de quatre.
+                                La liberté du joueur est surtout dans la décision d'appliquer des obstacles à l'adversaire avec le risque, si celui-ci possède la protection spéciale, qu'il marque 300 points supplémentaires comme « coup fourré ». 
+                                Bien sûr, ce risque augmente au cours du jeu au fur et à mesure que plus de cartes sont piochées.
+                                Le jeu se joue sur table, avec une pioche et une défausse centrale. 
+                                Chaque joueur a devant lui ces cartes de distance, visible de tous, ainsi que sa 
+                                pile de jeu, sur laquelle sont empilées au fur et à mesure les cartes d'attaque et de 
+                                parade. Chaque joueur doit commencer par poser un feu vert pour pouvoir rouler et poser 
+                                des cartes de distance. A son tour, un joueur joue une carte (ou en défausse une, s'il ne 
+                                peut rien jouer), puis en re-pioche une, de manière à toujours en avoir 6 en main. 
+                                Les kilomètres, parades et bottes sont jouées pour soi, les attaques sont jouées chez les adversaires pour les ralentir 
+                                voire les arrêter.
+                            </p> <br /> <br /> <br />
+
+                            <strong> Attaque </strong>
+                            <p>
+                                Il y a cinq attaques différentes : l'accident, la crevaison, la panne d'essence, la limite de vitesse et le feu rouge.
+                                L'accident, la crevaison et la panne d'essence immobilisent le véhicule : le joueur à qui on pose une de ces attaques ne peut plus poser aucune carte étape. 
+                                Pour redémarrer, le joueur doit jouer la parade correspondant
+                                à l'attaque, puis jouer un feu vert pour repartir et pouvoir recommencer à rouler.
+                                Le feu rouge empêche également de continuer à avancer (on ne peut plus poser de cartes étapes), mais il se pare simplement avec un feu vert.
+                                La limite de vitesse est une attaque qui peut se superposer aux autres. Elle n'empêche pas d'avancer, mais elle oblige le joueur à ne poser que des cartes de 25 ou 50km, ce qui ralentit beaucoup le jeu. 
+                                On peut avoir une limitation de vitesse même en étant à l'arrêt. Elle se pare simplement avec une fin de limite de vitesse.
+                            </p> <br /> <br /> <br />
+
+                            <strong> Parade </strong>
+                            <p>
+                                Il y a cinq parades différentes, qui correspondent chacune à une attaque (respectivement) : les réparations, l'essence, la roue de secours, la fin de limite de vitesse et le feu vert.
+                                Ces cartes sont les plus utiles à conserver en main, afin de pouvoir parer rapidement une attaque et repartir. On ne peut jouer la parade que sur l'attaque qui lui correspond, et en réponse
+                                à cette attaque (on ne peut pas se prémunir d'un éventuel accident en jouant une réparation en avance).
+                                Le feu vert est la carte la plus disponible (14 exemplaires) et la plus importante du jeu. En effet, non seulement les joueurs doivent en jouer un au début de la partie pour commencer à rouler, 
+                                mais ils doivent aussi en jouer un après une réparation, une essence ou une roue de secours pour pouvoir repartir
+                            </p> <br /> <br /> <br />
+
+                            <strong> Botte </strong>
+                            <p> Les bottes sont des cartes spéciales, qui permettent de se prémunir contre un type d'attaque. Lors qu'un joueur a une botte devant lui, il est impossible de lui mettre l'attaque parée par cette botte. 
+                                Cette carte est permanente, et un joueur peut avoir plusieurs bottes.
+                                <ul>L'as du volant ne peut pas avoir d'accident. </ul>
+                                <ul>Le camion-citerne ne peut pas tomber en panne d'essence. </ul>
+                                <ul>L'increvable ne peut pas crever. </ul>
+                                <ul>Le véhicule prioritaire ne peut pas avoir de limite de vitesse ou de feu rouge. Par ailleurs, le véhicule prioritaire n'a pas besoin de feu vert pour redémarrer après un accident, une panne ou une crevaison,
+                                    ni en début de partie avant de poser sa première borne. </ul>
+                                Il est possible de jouer une botte dès qu'on l'a en main, pour s'éviter des attaques futures. Mais il est aussi possible de la conserver en main. Dans ce cas, au moment où le joueur se fait attaquer par une carte 
+                                qu'il peut botter (on lui met une crevaison alors qu'il a l'increvable en main, par ex.), il s'agit d'un « coup fourré »
+                                (qui vaut 300 points). Le joueur attaqué annonce le coup fourré à haute voix et joue immédiatement sa botte. L'attaque jouée est défaussée, le joueur venant de faire le coup fourré pioche une carte. 
+                                Le joueur ayant posé l'attaque termine son tour et pioche sa carte, et c'est au tour du joueur suivant, dans l'ordre normal de jeu.
+                            </p>
+                        </p>
+                        </div>
+                    )}
+                
 
                 {playerList.map((player, index) => (
                     <div className={`MB-adversaire-container MB-p${playerList.length === 2 ? "trio" : index + 1}  ${player.myTurn ? "myTurn" : ""}`} key={index}>
